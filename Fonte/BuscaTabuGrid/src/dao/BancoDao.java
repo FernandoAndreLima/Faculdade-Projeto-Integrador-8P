5486573f240fc3998b88b25a869c04b27d5bb890
nosrednawall <nosrednawall@gmail.com>
2019-09-29 19:30:03 -0300

    aprendendo a ordenar em java

--- Fonte/BuscaTabuGrid/src/dao/BancoDao.java
+++ Fonte/BuscaTabuGrid/src/dao/BancoDao.java
@@ -16,116 +16,81 @@ import enums.RegimeEnum;
 import enums.SemestreEnum;
 
 public class BancoDao {
-	
-	static Set<Professor> professores;
-	static Set<Disciplina> disciplinas;
-	static Set<Curso> cursos;
+
+	public Set<Professor> professores = new HashSet<>();
+	public Set<Disciplina> disciplinas = new HashSet<>();
+	public Set<Curso> cursos = new HashSet<>();
 
 	/*
 	 * Pensar no que fazer com isso
 	 */
-	public static boolean generateData() {
-		
-		boolean bancoPopulado = false;
-		
-		professores = new HashSet<>();
-		disciplinas = new HashSet<>();
-		cursos = new HashSet<>();
-		
-		populaCurso();
-		populaDisciplinas();
-		populaProfessores();
-		
-		//adiciona os professores na disciplinas
-		
-		populaDisciplinas().disciplina1.addProfessor(populaProfessores().professor1);
-		disciplina1.addProfessor(professor2);
-		disciplina2.addProfessor(professor3);
-		disciplina3.addProfessor(professor1);
-		disciplina4.addProfessor(professor3);
-		disciplina5.addProfessor(professor1);
-		disciplina5.addProfessor(professor3);
+	public boolean generateData() {
 
-		//adiciona disciplina no professor
-		professor1.addDisciplina(disciplina1);
-		
-		
-		//adiciona curso na disciplina
-		disciplina1.addCurso(curso1);
-		
-		//adiciona as disciplinas nos cursos
-		curso1.addDisciplina(disciplina1);
-		
-		//popula os arrays
-		professores.add(professor1);
+		if (professores.isEmpty() && disciplinas.isEmpty() && cursos.isEmpty()) {
 
-		disciplinas.add(disciplina1);
-		
-		return (professores.size() > 0 && disciplinas.size() > 0 && cursos.size() > 0) ? true: false;
+			populaBanco();
+		}
+
+		System.out.println(professores.isEmpty());
+
+		return (professores.size() > 0 && disciplinas.size() > 0 && cursos.size() > 0) ? true : false;
 	}
-	
-	private static void populaCurso() {
-		//cria os cursos
+
+	private void populaBanco() {
+
+		// cria os cursos
 		Curso curso1 = new Curso();
 		curso1.setId(new Long(1));
-		curso1.setNome("Bacharelado em sistemas da informação");		
+		curso1.setNome("Bacharelado em sistemas da informação");
 		curso1.setTempoDuracao("4");
 		curso1.setDescricao("");
-		
-		cursos.add(curso1);
-	}
-	
-	private static void populaDisciplinas() {
 
-		
-	}
-	
-	private static void populaProfessores() {
+		this.cursos.add(curso1);
 
-		//cria as disciplinas
+		// cria as disciplinas
 		Disciplina disciplina1 = new Disciplina();
 		disciplina1.setId(new Long(1));
 		disciplina1.setNome("Sistemas de Informação");
 		disciplina1.setCargaHoraria("80");
 		disciplina1.setDescricao("");
 
-		//cria as disciplinas
+		// cria as disciplinas
 		Disciplina disciplina2 = new Disciplina();
 		disciplina2.setId(new Long(2));
 		disciplina2.setNome("Java 1");
 		disciplina2.setCargaHoraria("80");
 		disciplina2.setDescricao("");
-		
-		//cria as disciplinas
+
+		// cria as disciplinas
 		Disciplina disciplina3 = new Disciplina();
 		disciplina3.setId(new Long(3));
 		disciplina3.setNome("Projeto Integrador");
 		disciplina3.setCargaHoraria("80");
 		disciplina3.setDescricao("");
-		
-		//cria as disciplinas
+
+		// cria as disciplinas
 		Disciplina disciplina4 = new Disciplina();
 		disciplina4.setId(new Long(4));
 		disciplina4.setNome("Programação de Computadores");
 		disciplina4.setCargaHoraria("80");
 		disciplina4.setDescricao("");
-		
-		//cria as disciplinas
+
+		// cria as disciplinas
 		Disciplina disciplina5 = new Disciplina();
 		disciplina5.setId(new Long(5));
 		disciplina5.setNome("Algoritmos e Estruturas de Dados");
 		disciplina5.setCargaHoraria("80");
 		disciplina5.setDescricao("");
-		
-		//cria os professores
+
+		// cria os professores
 		Professor professor1 = new Professor();
 		professor1.setId(new Long(1));
 		professor1.setNomeCompleto("Arsène Lupin");
 		professor1.setRegime(RegimeEnum.NOITE);
 		professor1.setCargo(CargoEnum.PROFESSOR);
 		professor1.setFormacao(FormacaoEnum.BACHAREL);
-		
-		//disponibilidade professor 01
+
+		// disponibilidade professor 01
 		DisponibilidadeProfessor dispoUmProfe01 = new DisponibilidadeProfessor();
 		dispoUmProfe01.setId(new Long(1));
 		dispoUmProfe01.setAno("2019");
@@ -135,7 +100,7 @@ public class BancoDao {
 		dispoUmProfe01.addDiasDisponiveis(DiasEnum.TERCA_FEIRA);
 		dispoUmProfe01.addDiasDisponiveis(DiasEnum.QUARTA_FEIRA);
 		dispoUmProfe01.addDiasDisponiveis(DiasEnum.SEXTA_FEIRA);
-		//dia nao disponivel
+		// dia nao disponivel
 		DiaNaoDisponivel dNDProfe01 = new DiaNaoDisponivel();
 		dNDProfe01.setDia(DiasEnum.QUINTA_FEIRA);
 		dNDProfe01.setDisponibilidade(dispoUmProfe01);
@@ -143,8 +108,8 @@ public class BancoDao {
 		dNDProfe01.setId(new Long(1));
 		dNDProfe01.setMotivo("Trabalho voluntário");
 		dispoUmProfe01.addDiasNaoDisponiveis(dNDProfe01);
-		
-		//disponibilidade 02 professor 01
+
+		// disponibilidade 02 professor 01
 		DisponibilidadeProfessor dispoDoisProfe01 = new DisponibilidadeProfessor();
 		dispoDoisProfe01.setId(new Long(2));
 		dispoDoisProfe01.setAno("2019");
@@ -154,7 +119,7 @@ public class BancoDao {
 		dispoDoisProfe01.addDiasDisponiveis(DiasEnum.QUARTA_FEIRA);
 		dispoDoisProfe01.addDiasDisponiveis(DiasEnum.QUINTA_FEIRA);
 		dispoDoisProfe01.addDiasDisponiveis(DiasEnum.SEXTA_FEIRA);
-		//dia nao disponivel
+		// dia nao disponivel
 		DiaNaoDisponivel dNDProfe01Segundo = new DiaNaoDisponivel();
 		dNDProfe01Segundo.setDia(DiasEnum.TERCA_FEIRA);
 		dNDProfe01Segundo.setDisponibilidade(dispoUmProfe01);
@@ -162,16 +127,16 @@ public class BancoDao {
 		dNDProfe01Segundo.setId(new Long(1));
 		dNDProfe01Segundo.setMotivo("Trabalho voluntário");
 		dispoDoisProfe01.addDiasNaoDisponiveis(dNDProfe01Segundo);
-		
-		//cria os professores
+
+		// cria os professores
 		Professor professor2 = new Professor();
 		professor2.setId(new Long(2));
 		professor2.setNomeCompleto("Miss Marple");
 		professor2.setRegime(RegimeEnum.NOITE);
 		professor2.setCargo(CargoEnum.PROFESSOR);
 		professor2.setFormacao(FormacaoEnum.BACHAREL);
-		
-		//disponibilidade professor 02
+
+		// disponibilidade professor 02
 		DisponibilidadeProfessor dispoUmProfe02 = new DisponibilidadeProfessor();
 		dispoUmProfe02.setAno("2019");
 		dispoUmProfe02.setSemestre(SemestreEnum.primeiro);
@@ -180,15 +145,15 @@ public class BancoDao {
 		dispoUmProfe02.addDiasDisponiveis(DiasEnum.QUARTA_FEIRA);
 		dispoUmProfe02.addDiasDisponiveis(DiasEnum.QUINTA_FEIRA);
 		dispoUmProfe02.addDiasDisponiveis(DiasEnum.SEXTA_FEIRA);
-		//dia nao disponivel
+		// dia nao disponivel
 		DiaNaoDisponivel dNDProfe02 = new DiaNaoDisponivel();
 		dNDProfe02.setDia(DiasEnum.SEGUNDA_FEIRA);
 		dNDProfe02.setDisponibilidade(dispoUmProfe01);
 		dNDProfe02.setGrauMotivo(GrauMotivoEnum.ALTO);
 		dNDProfe02.setMotivo("Trabalho em outra instituicao");
 		dispoUmProfe01.addDiasNaoDisponiveis(dNDProfe02);
-		
-		//disponibilidade 02 professor 02
+
+		// disponibilidade 02 professor 02
 		DisponibilidadeProfessor dispoDoisProfe02 = new DisponibilidadeProfessor();
 		dispoDoisProfe02.setAno("2019");
 		dispoDoisProfe02.setSemestre(SemestreEnum.segundo);
@@ -197,98 +162,98 @@ public class BancoDao {
 		dispoDoisProfe02.addDiasDisponiveis(DiasEnum.TERCA_FEIRA);
 		dispoDoisProfe02.addDiasDisponiveis(DiasEnum.QUARTA_FEIRA);
 		dispoDoisProfe02.addDiasDisponiveis(DiasEnum.QUINTA_FEIRA);
-		//dia nao disponivel
+		// dia nao disponivel
 		DiaNaoDisponivel dNDProfe02Segundo = new DiaNaoDisponivel();
 		dNDProfe02Segundo.setDia(DiasEnum.SEXTA_FEIRA);
 		dNDProfe02Segundo.setDisponibilidade(dispoUmProfe01);
 		dNDProfe02Segundo.setGrauMotivo(GrauMotivoEnum.ALTO);
 		dNDProfe02Segundo.setMotivo("Trabalho voluntário");
 		dispoDoisProfe02.addDiasNaoDisponiveis(dNDProfe02Segundo);
-		
-				
-		//cria os professores
+
+		// cria os professores
 		Professor professor3 = new Professor();
 		professor3.setNomeCompleto("Arthur Conan Doyle");
 		professor3.setRegime(RegimeEnum.NOITE);
 		professor3.setCargo(CargoEnum.PROFESSOR);
 		professor3.setFormacao(FormacaoEnum.BACHAREL);
-		
-		//disponibilidade professor 02
+
+		// disponibilidade professor 02
 		DisponibilidadeProfessor dispoUmProfe03 = new DisponibilidadeProfessor();
 		dispoUmProfe03.setAno("2019");
 		dispoUmProfe03.setSemestre(SemestreEnum.primeiro);
 		dispoUmProfe03.setProfessor(professor2);
 		dispoUmProfe03.addDiasDisponiveis(DiasEnum.QUINTA_FEIRA);
 		dispoUmProfe03.addDiasDisponiveis(DiasEnum.SEXTA_FEIRA);
-		//dia nao disponivel
+		// dia nao disponivel
 		DiaNaoDisponivel dNDProfe03 = new DiaNaoDisponivel();
 		dNDProfe03.setDia(DiasEnum.SEGUNDA_FEIRA);
 		dNDProfe03.setDisponibilidade(dispoUmProfe01);
 		dNDProfe03.setGrauMotivo(GrauMotivoEnum.ALTO);
 		dNDProfe03.setMotivo("Trabalho em outra instituicao");
 		dispoUmProfe03.addDiasNaoDisponiveis(dNDProfe03);
-		
+
 		DiaNaoDisponivel dNDDoisProfe03 = new DiaNaoDisponivel();
 		dNDDoisProfe03.setDia(DiasEnum.TERCA_FEIRA);
 		dNDDoisProfe03.setDisponibilidade(dispoUmProfe01);
 		dNDDoisProfe03.setGrauMotivo(GrauMotivoEnum.BAIXO);
 		dNDDoisProfe03.setMotivo("Descanso");
 		dispoUmProfe03.addDiasNaoDisponiveis(dNDDoisProfe03);
-		
-		//disponibilidade 02 professor 02
-		DisponibilidadeProfessor dispoDoisProfe03 = new DisponibilidadeProfessor();
-		dispoDoisProfe03.setAno("2019");
-		dispoDoisProfe03.setSemestre(SemestreEnum.segundo);
-		dispoDoisProfe03.setProfessor(professor2);
-		dispoDoisProfe03.addDiasDisponiveis(DiasEnum.SEGUNDA_FEIRA);
-		dispoDoisProfe03.addDiasDisponiveis(DiasEnum.TERCA_FEIRA);
-		dispoDoisProfe03.addDiasDisponiveis(DiasEnum.QUARTA_FEIRA);
-		dispoDoisProfe03.addDiasDisponiveis(DiasEnum.QUINTA_FEIRA);
-		
-		//dia nao disponivel
-		DiaNaoDisponivel dNDProfe02Segundo = new DiaNaoDisponivel();
-		dNDProfe02Segundo.setDia(DiasEnum.SEXTA_FEIRA);
-		dNDProfe02Segundo.setDisponibilidade(dispoUmProfe01);
-		dNDProfe02Segundo.setGrauMotivo(GrauMotivoEnum.ALTO);
-		dNDProfe02Segundo.setId(new Long(1));
-		dNDProfe02Segundo.setMotivo("Trabalho voluntário");
-		dispoDoisProfe02.addDiasNaoDisponiveis(dNDProfe02Segundo);
-		
-		//cria as disciplinas
-		Disciplina disciplina1 = new Disciplina();
-		disciplina1.setId(new Long(1));
-		disciplina1.setNome("Sistemas de Informação");
-		disciplina1.setCargaHoraria("80");
-		disciplina1.setDescricao("");
 
-		//cria as disciplinas
-		Disciplina disciplina2 = new Disciplina();
-		disciplina2.setId(new Long(2));
-		disciplina2.setNome("Java 1");
-		disciplina2.setCargaHoraria("80");
-		disciplina2.setDescricao("");
-		
-		//cria as disciplinas
-		Disciplina disciplina3 = new Disciplina();
-		disciplina3.setId(new Long(3));
-		disciplina3.setNome("Projeto Integrador");
-		disciplina3.setCargaHoraria("80");
-		disciplina3.setDescricao("");
-		
-		//cria as disciplinas
-		Disciplina disciplina4 = new Disciplina();
-		disciplina4.setId(new Long(4));
-		disciplina4.setNome("Programação de Computadores");
-		disciplina4.setCargaHoraria("80");
-		disciplina4.setDescricao("");
-		
-		//cria as disciplinas
-		Disciplina disciplina5 = new Disciplina();
-		disciplina5.setId(new Long(5));
-		disciplina5.setNome("Algoritmos e Estruturas de Dados");
-		disciplina5.setCargaHoraria("80");
-		disciplina5.setDescricao("");
-		
+		// dia nao disponivel
+		DiaNaoDisponivel dNDTresProfe03 = new DiaNaoDisponivel();
+		dNDTresProfe03.setDia(DiasEnum.QUARTA_FEIRA);
+		dNDTresProfe03.setDisponibilidade(dispoUmProfe01);
+		dNDTresProfe03.setGrauMotivo(GrauMotivoEnum.MEDIO);
+		dNDTresProfe03.setMotivo("Estudo auto diciplinar");
+		dispoDoisProfe02.addDiasNaoDisponiveis(dNDTresProfe03);
+
+		// adiciona os professores na disciplinas
+		disciplina1.addProfessor(professor1);
+		disciplina1.addProfessor(professor2);
+		disciplina2.addProfessor(professor3);
+		disciplina3.addProfessor(professor1);
+		disciplina4.addProfessor(professor3);
+		disciplina5.addProfessor(professor1);
+		disciplina5.addProfessor(professor3);
+
+		// adiciona disciplina no professor 1
+		professor1.addDisciplina(disciplina1);
+		professor1.addDisciplina(disciplina3);
+		professor1.addDisciplina(disciplina5);
+
+		professor2.addDisciplina(disciplina3);
+
+		professor3.addDisciplina(disciplina2);
+		professor3.addDisciplina(disciplina4);
+		professor3.addDisciplina(disciplina5);
+
+		// adiciona curso na disciplina
+		disciplina1.addCurso(curso1);
+		disciplina2.addCurso(curso1);
+		disciplina3.addCurso(curso1);
+		disciplina4.addCurso(curso1);
+		disciplina5.addCurso(curso1);
+
+		this.professores.add(professor1);
+		this.professores.add(professor2);
+		this.professores.add(professor3);
+
+		this.disciplinas.add(disciplina1);
+		this.disciplinas.add(disciplina2);
+		this.disciplinas.add(disciplina4);
+		this.disciplinas.add(disciplina3);
+		this.disciplinas.add(disciplina5);
 	}
 
+	public Set<Disciplina> getDisciplinas() {
+		return disciplinas;
+	}
+
+	public Set<Curso> getCursos() {
+		return cursos;
+	}
+
+	public Set<Professor> getProfessores() {
+		return professores;
+	}
 }
