c2c49e73ddb2b3728232baa2a495db561fb91bda
nosrednawall <nosrednawall@gmail.com>
2019-09-29 19:54:58 -0300

    backup

--- Fonte/BuscaTabuGrid/src/ordenacao/MainProduto.java
+++ Fonte/BuscaTabuGrid/src/ordenacao/MainProduto.java
@@ -3,57 +3,37 @@ package ordenacao;
 public class MainProduto {
 
 	public static void main(String[] args) {
-		Produto produtos[] = { 
-				new Produto("Lamborghini", 1000000), 
-				new Produto("Jipe", 46000),
-				new Produto("Brasilia", 16000),
-				new Produto("Smart", 46000), 
-				new Produto("Fusca", 17000) };
-		int[] maisBaratomaisCaro = verificaOMaisBaratoECaro(produtos, 0,4);
+		Produto produtos[] = { new Produto("Lamborghini", 1000000), new Produto("Jipe", 46000),
+				new Produto("Brasilia", 16000), new Produto("Smart", 46000), new Produto("Fusca", 17000) };
+		int maisCaro = carroMaisCaro(produtos, 0, 4);
+		int maisBararo = carroMaisBarato(produtos, 0, 4);
 
-		System.out.println(maisBaratomaisCaro[0]);
-		System.out.println("O produto mais barato é: " + produtos[maisBaratomaisCaro[0]].getNome() +
-				" com o valor de R$"+ produtos[maisBaratomaisCaro[0]].getValor());
-		
-		System.out.println(maisBaratomaisCaro[1]);
-		System.out.println("O produto mais caro é: " + produtos[maisBaratomaisCaro[1]].getNome() +
-				" com o valor de R$"+ produtos[maisBaratomaisCaro[1]].getValor());
+		System.out.println(maisBararo);
+		System.out.println("O produto mais barato é: " + produtos[maisBararo].getNome() + " com o valor de R$"
+				+ produtos[maisBararo].getValor());
+
+		System.out.println(maisCaro);
+		System.out.println("O produto mais caro é: " + produtos[maisCaro].getNome() + " com o valor de R$"
+				+ produtos[maisCaro].getValor());
 	}
 
-	public static int [] verificaOMaisBaratoECaro(Produto produtos[], int inicio, int termino) {
-
-		int maisBarato = 0;
-		int maisCaro = 0;
-		int[] retorno = new int[2];
-		
-		for (int atual = inicio; atual <= termino; atual++) {
-			if (produtos[atual].getValor() < produtos[maisBarato].getValor()) {
-				maisBarato = atual; retorno[0] = maisBarato;
-			}
-			else if(produtos[atual].getValor() > produtos[maisCaro].getValor()) {
-				maisCaro = atual; retorno[1] = maisCaro;
-			}
-		}
-		return retorno;
-	}
-	
 	public static int carroMaisBarato(Produto produtos[], int inicio, int termino) {
 		int maisBarato = 0;
-		
+
 		for (int atual = inicio; atual <= termino; atual++) {
-			if (produtos[atual].getValor() < produtos[maisBarato].getValor()) 
-				maisBarato = atual; 
+			if (produtos[atual].getValor() < produtos[maisBarato].getValor())
+				maisBarato = atual;
 
 		}
 		return maisBarato;
 	}
-	
+
 	public static int carroMaisCaro(Produto produtos[], int inicio, int termino) {
 		int maisCaro = 0;
-		
+
 		for (int atual = inicio; atual <= termino; atual++) {
-			if (produtos[atual].getValor() > produtos[maisCaro].getValor()) 
-				maisCaro = atual; 
+			if (produtos[atual].getValor() > produtos[maisCaro].getValor())
+				maisCaro = atual;
 
 		}
 		return maisCaro;
@@ -62,29 +42,20 @@ public class MainProduto {
 
 class Produto {
 
-	private int valor;
+	private long valor;
 	private String nome;
 
-	public Produto(String nome, int valor) {
-		super();
+	public Produto(String nome, long valor) {
 		this.valor = valor;
 		this.nome = nome;
 	}
 
-	public int getValor() {
+	public long getValor() {
 		return valor;
-	}
-
-	public void setValor(int valor) {
-		this.valor = valor;
 	}
 
 	public String getNome() {
 		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
 	}
 
 }
