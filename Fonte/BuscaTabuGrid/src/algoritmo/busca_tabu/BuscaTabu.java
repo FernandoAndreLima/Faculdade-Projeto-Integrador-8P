d6ddcec4e833e45dfd8bdd1a42adec14cf23485a
nosrednawall <nosrednawall@gmail.com>
2019-10-13 11:00:34 -0300

    backup

--- Fonte/BuscaTabuGrid/src/algoritmo/busca_tabu/BuscaTabu.java
+++ Fonte/BuscaTabuGrid/src/algoritmo/busca_tabu/BuscaTabu.java
@@ -1,22 +1,87 @@
 package algoritmo.busca_tabu;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BuscaTabu {
 
-	
-}
-class ListaTabu {
-	
 }
 
-class ObjetoTabu{
-	private int idadeObjetoBT;
+class ListaTabu {
+	private Set<ObjetoTabu> lista = new HashSet<>();
+
+	public ListaTabu() {
+		if (lista.isEmpty())
+			this.lista = new HashSet<>();
+	}
+
+	public void adicionaItem(ObjetoTabu objeto) {
+		this.lista.add(objeto);
+	}
+
+	public ObjetoTabu buscaObjeto(int idItem) {
+		int cont = 0;
+		for (ObjetoTabu objetoTabu : lista) {
+			if (cont == idItem) {
+				return objetoTabu;
+			} else {
+				cont++;
+			}
+		}
+		return null;
+	}
+
+	public boolean contemObjeto(int idItem) {
+		int cont = 0;
+		for (ObjetoTabu objetoTabu : lista) {
+			if (cont == idItem) {
+				return true;
+			} else {
+				cont++;
+			}
+		}
+		return false;
+	}
 	
+	public boolean removeItem(int idItem) {
+		if(contemObjeto(idItem))
+			lista.remove(buscaObjeto(idItem));
+		
+		return !contemObjeto(idItem)? true : false;
+	}
+}
+
+class ObjetoTabu {
+
+	private int id;
+
+	private int idadeObjetoBT;
+
 	private ObjetoGradeCurricular objeto;
+
+	private int[] gradeCurricularBinaria;
 
 	public ObjetoTabu(int idadeObjetoBT, ObjetoGradeCurricular objeto) {
 		super();
 		this.idadeObjetoBT = idadeObjetoBT;
 		this.objeto = objeto;
+		this.gradeCurricularBinaria = converteObjetoGradeEmObjetoBinario(objeto);
+	}
+
+	/*
+	 * estou pensando em utilizar objetos mesmo
+	 */
+	private int[] converteObjetoGradeEmObjetoBinario(ObjetoGradeCurricular objeto) {
+		
+		int[] array = {
+				objeto.getId(),
+				objeto.getDia(),
+				objeto.getDisciplina(),
+				objeto.getProfessor(),
+				objeto.getCurso()
+		};
+		
+		return array;
 	}
 
 	public int getIdadeObjetoBT() {
@@ -27,15 +92,19 @@ class ObjetoTabu{
 		return objeto;
 	}
 
+	public int[] getGradeCurricularBinaria() {
+		return gradeCurricularBinaria;
+	}
+
 }
 
-class ObjetoGradeCurricular{
+class ObjetoGradeCurricular {
 	private int id;
 	private int dia;
 	private int professor;
 	private int disciplina;
 	private int curso;
-		
+
 	public ObjetoGradeCurricular(int id, int dia, int professor, int disciplina, int curso) {
 		super();
 		this.id = id;
@@ -48,19 +117,21 @@ class ObjetoGradeCurricular{
 	public int getId() {
 		return id;
 	}
+
 	public int getDia() {
 		return dia;
 	}
+
 	public int getProfessor() {
 		return professor;
 	}
+
 	public int getDisciplina() {
 		return disciplina;
 	}
+
 	public int getCurso() {
 		return curso;
 	}
-	
-	
-	
+
 }
