ee22437a1e96b7566df536a79ecafa169c26615b
nosrednawall <nosrednawall@gmail.com>
2019-10-26 18:40:16 -0300

    backup

--- Fonte/BuscaTabuGrid/src/Main.java
+++ Fonte/BuscaTabuGrid/src/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		dao.generateData();
 
 		Set<Professor> professores = new HashSet<>();
-		
+
 		Set<Disciplina> disciplinasPrimeiro = new HashSet<>();
 		Set<Disciplina> disciplinasSegundo = new HashSet<>();
 		Set<Disciplina> disciplinasTerceiro = new HashSet<>();
@@ -26,18 +26,19 @@ public class Main {
 		Set<Disciplina> disciplinasSexto = new HashSet<>();
 		Set<Disciplina> disciplinasSetimo = new HashSet<>();
 		Set<Disciplina> disciplinasOitavo = new HashSet<>();
-		
+
 		Set<Curso> cursos = new HashSet<>();
 
 		professores.addAll(dao.getProfessores());
 		disciplinasPrimeiro.addAll(dao.getDisciplinas());
-		cursos.addAll(dao.getCursos());	
+		cursos.addAll(dao.getCursos());
 
 		System.out.println(professores.isEmpty());
 
-		GradeHoraria gradePrimeiro = Resolvedor.constroiGrade(new GradeHoraria(Periodo.PRIMEIRO, "2019", professores, disciplinasPrimeiro));
-		GradeHoraria gradeSegundo = Resolvedor.constroiGrade(new GradeHoraria(Periodo.TERCEIRO, "2019", professores, disciplinasSegundo));
-		
+		GradeHoraria gradePrimeiro = Resolvedor
+				.constroiGrade(new GradeHoraria(Periodo.PRIMEIRO, "2019", professores, disciplinasPrimeiro));
+		GradeHoraria gradeSegundo = Resolvedor
+				.constroiGrade(new GradeHoraria(Periodo.TERCEIRO, "2019", professores, disciplinasSegundo));
 
 	}
 }
--- Fonte/BuscaTabuGrid/src/algoritmo/Resolvedor.java
+++ Fonte/BuscaTabuGrid/src/algoritmo/Resolvedor.java
@@ -1,9 +1,7 @@
 package algoritmo;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import entities.Disciplina;
 import entities.Professor;
